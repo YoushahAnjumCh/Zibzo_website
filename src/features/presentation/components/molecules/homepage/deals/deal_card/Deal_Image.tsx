04ebcb8da9a53f0cbd0e7b3614f768c584598b72
Youshah <mohdyoushah@gmail.com>
2024-12-18 17:53:01 +0530

    deal of day image height issue resolved

--- src/features/presentation/components/molecules/homepage/deals/deal_card/Deal_Image.tsx
+++ src/features/presentation/components/molecules/homepage/deals/deal_card/Deal_Image.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import CustomImage from "../../../../atoms/image/CustomImage";
-import ApiService from "../../../../../../../constant/Environment";
 
 type DealImageProps = {
   image: string;
   title: string;
+  logo?: string;
 };
 
 export default function Deal_Image(props: DealImageProps) {
+  const imageHeightClass = props.logo?.trim()
+    ? "md:h-[190px] h-[130px]"
+    : "md:h-[220px] h-[160px]";
+
   return (
     <>
       <CustomImage
         ImageSrc={props.image}
         alt={props.title}
-        className=" bg-gray-100 md:h-[190px] h-[130px] w-full  object-center rounded-t-lg"
+        className={`bg-gray-100 ${imageHeightClass} w-full object-center rounded-t-lg`}
       />
     </>
   );
